fcfbdcd342e05b0f4ca63d5cd095b75215ddb221
ChinhVo853 <chinhvo853@gmail.com>
2024-09-27 15:10:21 +0700

    create_migration_1

--- database/migrations/2024_09_26_141531_create_product_type_table.php
+++ database/migrations/2024_09_26_141531_create_product_type_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('product_type', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('product_type');
     }
 };
